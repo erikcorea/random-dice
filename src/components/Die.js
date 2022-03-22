30457992769829f3a635978db2b0407c54af0622
erikcorrea444@gmail.com <erikcorrea444@gmail.com>
2022-03-21 20:40:47 -0400

    added animation

--- src/components/Die.js
+++ src/components/Die.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import '../styling/die.css';
 
-export default function Die({face}) {
+export default function Die({face, roll}) {
   return (
-    <i className={`die fas fa-dice-${face}`} />
+    <i className={`die fas fa-dice-${face} ${roll ? "roll" : "" }`} />
   )
 }
--- src/components/RollDice.js
+++ src/components/RollDice.js
@@ -7,6 +7,7 @@ export default function RollDice() {
 
     const [die1, setDie1] = useState("one");
     const [die2, setDie2] = useState("one");
+    const [rolling, setRolling] = useState(false);
 
     function roll(){
         const newDie1 = nums[Math.floor(Math.random() * nums.length)];
@@ -14,16 +15,21 @@ export default function RollDice() {
 
         setDie1(newDie1);
         setDie2(newDie2);
+        setRolling(true);
+
+        setTimeout(() => {
+            setRolling(false)
+        }, 1000)
     }
 
     
   return (
     <div className='rolldice'>
         <div className='dice-container'>
-            <Die face={die1} />
-            <Die face={die2} />
+            <Die face={die1} roll={rolling} />
+            <Die face={die2} roll={rolling} />
         </div>
-        <button onClick={roll}>Roll Dice</button>
+        <button onClick={roll} disabled={rolling}>{rolling ? "Rolling..." : "Roll Dice!"}</button>
     </div>
   )
 }
